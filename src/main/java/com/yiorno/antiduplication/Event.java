afe860379677c4108f34bd90774d02c0e5e1a357
yossy1zip <yossy@yiorno.com>
2023-08-13 19:51:27 +0900

    名前判定処理順序変更, _Kisame_RESPECT

--- src/main/java/com/yiorno/antiduplication/Event.java
+++ src/main/java/com/yiorno/antiduplication/Event.java
@@ -29,21 +29,24 @@ public class Event implements Listener {
 
         if (e.getCurrentItem().getType() != Material.AIR) {
 
+            //clicked
             String displayName = e.getCurrentItem().getItemMeta().getDisplayName();
 
             if (e.getInventory().getType() == InventoryType.ANVIL) {
 
+                //anvilの結果欄
                 if (e.getSlotType() == InventoryType.SlotType.RESULT) {
 
                     //元アイテムと違う名前になっている
                     //元アイテムにエンチャントがついている
                     //キャンセル
-                    if(latestItem.get(p).getItemMeta() == null) {
-                        return;
-                    }
+                    //if(latestItem.get(p).getItemMeta() == null) {
+                    //    return;
+                    //}
 
-                    if ((displayName != latestItem.get(p).getItemMeta().getDisplayName()) &&
-                            !(latestItem.get(p).getEnchantments().isEmpty())){
+                    //チェストの名前変更禁止
+                    //Vehiclesコピー対策
+                   if (e.getCurrentItem().getType().equals(Material.CHEST)){
 
                         //p.sendMessage("" + e.getCurrentItem().getType().name());
                         p.sendMessage(ChatColor.YELLOW + "このアイテムは名前の変更ができません！");
@@ -51,12 +54,20 @@ public class Event implements Listener {
                         return;
                     }
 
+                    //[&4悪魔の血液]作成キャンセル
+                    if (displayName.contains("悪魔の血液") || displayName.contains("黒弾")){
+                        p.sendMessage(ChatColor.RED + "アイテムからの魔力に弾かれた");
+                        e.setCancelled(true);
+                        return;
+                    }
+                    /*if (e.getCurrentItem().getType().name().equals("IRON_NUGGET") || e.getCurrentItem().getType().name().equals("RED_DYE")){
+                        p.sendMessage(ChatColor.YELLOW + "これを触るのはまずいようだ...");
+                        e.setCancelled(true);
+                        return;
+                    }*/
 
-                    //チェストの名前変更禁止
-                    //Vehiclesコピー対策
-                    //if (e.getCurrentItem().getType().name().toLowerCase().contains("chest") &&
-                    //!e.getCurrentItem().getType().name().toLowerCase().contains("trap")){
-                    if (e.getCurrentItem().getType().name().equals("CHEST")) {
+                    if ((displayName != latestItem.get(p).getItemMeta().getDisplayName()) &&
+                            !(latestItem.get(p).getEnchantments().isEmpty())){
 
                         //p.sendMessage("" + e.getCurrentItem().getType().name());
                         p.sendMessage(ChatColor.YELLOW + "このアイテムは名前の変更ができません！");
